e5a95bf3fbcf5bec6a996bdee2ab5c6e2a67cc25
Julio Rezende <juliorezende@bemol.com.br>
2020-08-13 13:44:04 -0400

    Fix: Ajuste para receber XP p1

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
-const Discord = require('discord.js');
+//libs
+var fs = require('fs');
+var Discord = require("discord.js");
+var bot = new Discord.Client();
 
-const bot = new Discord.Client();
+//auth... see authExample.json
+var auth = require("./auth.json")
 
-const token = 'NzQzMjQ3MjExMTY0NDY3Mjky.XzR46A.AsRRHcrRZRL0dAPhftAFqkmAq8Y';
+//stash
+var stash = require("./stash.json")
 
-bot.login(token)
+console.log('starting up...')
+bot.login(auth.token);
 
-bot.on('ready', () => {
-	console.log('estou pronto')
-})
+bot.on('ready', function() {
+    console.log(`Logged in as ${bot.user.tag}!`);
+});
 
-bot.on('message', msg =>{
-	if(msg.content === 'Teste bot Taverna'){
-		msg.reply('Olá! Funcionei e estou sendo testado na taverna destino!')
-	}
-})
+//status reports
+bot.on('message', function(msg) {
+    if (msg.content === '!help') {
+        msg.reply(`\n\nComandos disponiveis: !status_PrimeiroNome\n\nComandos de Adição:   +XP_PrimeiroNome **QTD**  \n\nComando Subtração:   -XP_PrimeiroNome **QTD**`)
+    }
+
+    if (msg.content === '!status_Gohdar') {
+        msg.reply(`Experiência:\n**${stash.XP_Gohdar} XP**`)
+    }
+
+});
+
+
+//modifiers + or -
+bot.on('message', function(msg) {
+    if (msg.content[0] === '+' || msg.content[0] === '-') {
+        var parseMsg = msg.content.split(' ')
+        var command = parseMsg[0]
+        var modifier = parseMsg[1]
+
+        if (command === '+XP_Gohdar') {
+            var amt = parseInt(modifier)
+            stash.XP_Gohdar += amt
+            fs.writeFileSync('stash.json', JSON.stringify(stash, null, 2));
+            msg.reply(`Adicionou **${parseMsg[1]}** XP!\nSeu Total atual é: **${stash.XP_Gohdar}** de XP`)
+
+        }
+
+        if (command === '-XP_Gohdar') {
+            var amt = parseInt(modifier)
+            stash.XP_Gohdar -= amt
+            fs.writeFileSync('stash.json', JSON.stringify(stash, null, 2));
+            msg.reply(`Subtraiu **${parseMsg[1]}** de XP!\nSeu Total atual é: **${stash.XP_Gohdar}** XP`)
+
+
+        }
+
+    }
+});
